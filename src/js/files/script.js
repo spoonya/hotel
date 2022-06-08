438d0b6317d19e25a6803c300c16f9e6ca2c5459
spoonya <nomad8t@gmail.com>
2022-06-08 17:52:40 +0300

    feat: implement location section

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,9 +1,7 @@
-// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+import { flsModules } from "./modules.js";
 import { controlHeaderBurger } from "./custom/burger.js";
 import { controlLangButton } from "./custom/lang-button.js";
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
 
 controlHeaderBurger();
 controlLangButton();
